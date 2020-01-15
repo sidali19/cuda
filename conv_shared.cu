6a95ff71f444c9a0a90a2a30ae2ea7adcd50335f
Sid Ali AIOULA <es_aioula@esi.dz>
2020-01-15 22:51:10 +0100

    Update conv_shared.cu

--- conv_shared.cu
+++ conv_shared.cu
@@ -130,6 +130,8 @@ int main(void)
 tilingKernelProcessing <<< grid, threadBlock >>>(gpu_data_in, gpu_mask,gpu_data_out,desired_channels,height, width);
 	cudaEventRecord(start);
 	cudaMemcpy (data_out, gpu_data_out, width * height * desired_channels, cudaMemcpyDeviceToHost);
+	
+	
 	cudaEventRecord(stop);
 	stbi_write_jpg("sortie.jpg", height, width, 1, data_out, height);
 	
