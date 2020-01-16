04cf961534f012d41bd68f5e9c9b853228c78ea8
Sid Ali AIOULA <es_aioula@esi.dz>
2020-01-16 21:14:27 +0100

    Update conv.cu

--- conv.cu
+++ conv.cu
@@ -11,116 +11,114 @@
 #include <cstdlib>
 #include <time.h>
 #include <math.h>
-
 #include "cuda_runtime.h"
 #define FILTRE_SIZE 3 
 
 
-__global__ 
-void PictureKernel (unsigned char* dPin, unsigned char* dPout, float *mask, int height, int width)
+__global__ void PictureKernel (unsigned char* dPin, unsigned char* dPout, float *mask, int height, int width)
 {	
 
-	// Compute row and column number of dPin and dPout element
-	const unsigned int col = blockIdx.y * blockDim.y + threadIdx.y;
-	const unsigned int row = blockIdx.x * blockDim.x + threadIdx.x;
-	int position = row*width+col ;
-	int up = position - width;
-	int down = position + width;
+		// Compute row and column number of dPin and dPout element
+		const unsigned int col = blockIdx.y * blockDim.y + threadIdx.y;
+		const unsigned int row = blockIdx.x * blockDim.x + threadIdx.x;
+		int position = row*width+col ;
 	// Each thread computes one element of dPout if in range
 	if(row==0||col==0||(row==width-1)||(row==height-1))
 	{
 		dPout[position] = dPin[position];
 		return;
-	}
-	
-
-
-	float val = (mask[0]*dPin[up-1] + mask[1]*dPin[up] + mask[2]*dPin[up + 1]
+	}	
+	float val = (mask[0]*dPin[position - width-1] + mask[1]*dPin[position - width] + mask[2]*dPin[position - width + 1]
 			+ mask[3]*dPin[position-1] + mask[4]*dPin[position] + mask[5]*dPin[position+1]
-			+ mask[6]*dPin[down -1] + mask[7]*dPin[down] + mask[8]*dPin[down+1]);
+			+ mask[6]*dPin[position + width -1] + mask[7]*dPin[position + width] + mask[8]*dPin[position + width+1]);
 	dPout[position] = (val <= 0 ? 0 : (val >= 255 ? 255 : (unsigned char)val));
 }
 
 void cuda_error(cudaError_t err,const char *file,int line) {
+	//cude check errors 
 	if (err != cudaSuccess){
 		printf("%s in %s at line %d\n" , cudaGetErrorString(err),
 			file, line);
 		exit(EXIT_FAILURE);
 	}
 }
-int main(void)
+#define cuda_error_check(err) (cuda_error( err, __FILE__, __LINE__ ))
+
+int main(int argc, char** argv)
 {
-
+        //declarations 
 		int width = 0, height = 0, nchannels = 0;
+		int num_iteration = 1000;
+		float executionTime ;
 		int const desired_channels = 1; // request to convert image to gray
-		char const * const filename = "im.jpg"; 
-	// Load the image 
-	unsigned char* data_in = stbi_load(filename, &width, &height, &nchannels, desired_channels);
+		char const * const filename1 = argv[1]; 
+		char const * const filename2 = "sortie.jpg"
+		// Load the image 
+		unsigned char* data_in = stbi_load(filename1, &width, &height, &nchannels, desired_channels);
+		// check for errors 
+		if (!data_in || !width || !height || !nchannels){
+		printf("Error loading image %s", filename1);
+		return -1;
+		}
 
-	// check for errors 
-	if (!data_in || !width || !height || !nchannels){
-	printf("Error loading image %s", filename);
-	return -1;
-	}
-
-	// the filter  
-	float mask[FILTRE_SIZE*FILTRE_SIZE] = { -1, -1, -1, -1, 8, -1, -1, -1, -1};
-
-
-	unsigned char*data_out = (unsigned char*)malloc(width * height * desired_channels);
-
-	// Memory allocation GPU
-	unsigned char *gpu_data_in, *gpu_data_out;
-	float * gpu_mask;
-	
-	cudaMalloc (( void **)&gpu_data_in, width * height * desired_channels*sizeof(float));
-	cudaMalloc (( void **)&gpu_data_out, width * height * desired_channels*sizeof(float));
-	cudaMalloc (( void **)&gpu_mask, FILTRE_SIZE*FILTRE_SIZE*sizeof(float));
-	
-	
-
-	cudaMemcpy (gpu_data_in, data_in, width * height * desired_channels*sizeof(float) , cudaMemcpyHostToDevice);
-	cudaMemcpy (gpu_mask, mask , FILTRE_SIZE*FILTRE_SIZE*sizeof(float), cudaMemcpyHostToDevice);
-	
+		// the filter mask 
+		float mask[FILTRE_SIZE*FILTRE_SIZE] = { -1, -1, -1, -1, 8, -1, -1, -1, -1};
 
 
-	// Set up the grid and block dimensions for the executions
-	const unsigned int block_col = 32;
-	const unsigned int block_row = 32;
-	cudaEvent_t start, stop;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
-	dim3 grid(height/block_col, width/ block_row, 1);
-   	 dim3 threadBlock(block_col, block_row, 1);
-  	cudaEventRecord(start, 0);
+	
 
-	PictureKernel <<< grid, threadBlock >>>(gpu_data_in, gpu_data_out,gpu_mask,height, width);
-	cudaEventRecord(stop, 0);
-	
-	cudaEventSynchronize(stop);
-	float executionTime ;
-	cudaEventElapsedTime(&executionTime, start, stop);
+		// Memory allocation GPU
+		unsigned char *gpu_data_in, *gpu_data_out;
+		unsigned char*data_out = (unsigned char*)malloc(width * height * desired_channels);
+		float * gpu_mask;
+		
+		cuda_error_check(cudaMalloc (( void **)&gpu_data_in, width * height * desired_channels*sizeof(unsigned char)));
+		cuda_error_check(cudaMalloc (( void **)&gpu_data_out, width * height * desired_channels*sizeof(unsigned char)));
+		cuda_error_check(cudaMalloc (( void **)&gpu_mask, FILTRE_SIZE*FILTRE_SIZE*sizeof(float)));
 
-	
-	
-	cudaMemcpy (data_out, gpu_data_out, width * height * desired_channels, cudaMemcpyDeviceToHost);
-	
-	printf("ExecutionTime: %f", executionTime);
-	//cudaEventRecord(stop);
-	stbi_write_jpg("sortie.jpg", height, width, 1, data_out, height);
-	
-	//  cudaEventSynchronize(stop);
-	
-  	//float milliseconds = 0;
-	
- 	// cudaEventElapsedTime(&milliseconds, start, stop);
-	
-       // printf("%f",milliseconds);
-	free(data_in);
-	free(data_out);
-	cudaFree(gpu_data_in);
-	cudaFree(gpu_data_out);
-	cudaFree(gpu_mask);
+		// data copy  host to device 
+		cuda_error_check(cudaMemcpy (gpu_data_in, data_in, width * height * desired_channels*sizeof(unsigned char) , cudaMemcpyHostToDevice));
+		cuda_error_check(cudaMemcpy (gpu_mask, mask , FILTRE_SIZE*FILTRE_SIZE*sizeof(float), cudaMemcpyHostToDevice));
+		
+		// Set up the grid and block dimensions for the executions
+		const unsigned int block_col = 32;
+		const unsigned int block_row = 32;
+		// creat cuda event to calculate time execution [start,stop]
+		cudaEvent_t start, stop;
+		cudaEventCreate(&start);
+		cudaEventCreate(&stop);
+		// grid dimension 
+		dim3 grid(height/block_col, width/ block_row, 1);
+		// block dimension 
+		dim3 threadBlock(block_col, block_row, 1);
+		// start recording    
+		cudaEventRecord(start, 0);
+		for(int i=0; i < num_iteration; i++){
+			// karnel call 
+				PictureKernel <<< grid, threadBlock >>>(gpu_data_in, gpu_data_out,gpu_mask,height, width);
+		}	
+		cudaEventRecord(stop, 0);
+		// stop recording 
+		cudaEventSynchronize(stop);
+		
+		cudaEventElapsedTime(&executionTime, start, stop);
+		// data copy  device to host
+		cuda_error_check(cudaMemcpy (data_out, gpu_data_out, width * height * desired_channels, cudaMemcpyDeviceToHost));
+		
+		printf("ExecutionTime: %f", executionTime);
+		printf("ExecutionTime image/seconde : %f", executionTime);
+		//write the image 
+		if(!stbi_write_jpg(filename2, height, width, 1, data_out, height))
+		{
+			printf("Error saving image %s \n", filename2);
+			return (-1);
+		}
+
+		free(data_in);
+		free(data_out);
+		cudaFree(gpu_data_in);
+		cudaFree(gpu_data_out);
+		cudaFree(gpu_mask);
 	
 
 }
